010852aa4eb17df52875e3fa82d40a8d865297e3
Chase Ashton <156870926+chashton404@users.noreply.github.com>
2025-02-25 18:05:02 -0700

    Created header in the app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body">App will display here</div>;
+  return (
+    <div className="body">
+      <header>
+        <div className="container">
+          <header className="d-flex flex-wrap justify-content-center pt-3">
+            <a href="landingPageBootstrap.html" className="d-flex align-items-center mb-0 text-decoration-none">
+              <h1>ROLLERSKATE RACING</h1>
+            </a>
+          </header>
+        </div>
+        <div className="divider"></div>
+      </header>
+
+      <main>App components will go here</main>
+
+    </div>
+  );
 }
