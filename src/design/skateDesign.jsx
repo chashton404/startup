18da40d1bbc913c954ad5f64cd45f116d0d5b61f
Chase Ashton <156870926+chashton404@users.noreply.github.com>
2025-02-27 10:04:03 -0700

    Created Skate Design Jsx

---
+++ src/design/skateDesign.jsx
@@ -0,0 +1,247 @@
+import React from 'react';
+
+export function SkateDesign() {
+  return (
+    <main>
+        <div className="container">
+            <div className="row pt-5">
+                <div className="col-md-6 justify-content-center">
+                    <div className="row-md-2">
+                        <form action="skateDesign.html" method="get">
+                            <div>  
+                                <input type="text" name="username" placeholder="Skate Name" className="skate-name-input"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="row-md-2 width: 100%">
+                        <div className="row-md-6">
+                            <h3>
+                                Top
+                            </h3>
+                        </div>
+                        <div className="row-md-6 d-flex justify-content-center">
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-black">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#000000" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-red">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#BF1E2D" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-orange">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#F1592A" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-blue">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#0F75BC" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-green">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#2BB673" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row-md-2">
+                        <div className="row-md-6">
+                            <h3>
+                                Stripes
+                            </h3>
+                        </div>
+                        <div className="row-md-6 d-flex justify-content-center">
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-black">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#000000" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-red">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#BF1E2D" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-orange">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#F1592A" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-blue">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#0F75BC"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-green">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#2BB673" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div><div className="row-md-2">
+                        <div className="row-md-6">
+                            <h3>
+                                Base
+                            </h3>
+                        </div>
+                        <div className="row-md-6 d-flex justify-content-center">
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-black">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#000000" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-red">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#BF1E2D" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-orange">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#F1592A" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-blue">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#0F75BC" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-green">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#2BB673" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div><div className="row-md-2">
+                        <div className="row-md-6">
+                            <h3>
+                                Wheels
+                            </h3>
+                        </div>
+                        <div className="row-md-6 d-flex justify-content-center">
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-black">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#000000" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-red">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#BF1E2D" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-orange">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#F1592A" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-blue">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#0F75BC"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-green">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#2BB673" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div><div className="row-md-2">
+                        <div className="row-md-6">
+                            <h3>
+                                Toe Stop
+                            </h3>
+                        </div>
+                        <div className="row-md-6 d-flex justify-content-center">
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-black">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#000000" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-red">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#BF1E2D" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-orange">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#F1592A" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-blue">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#0F75BC"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="selector-button-green">
+                                    <svg width="50" height="25" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="50" height="25" x="10" y="10" fill="#2BB673" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center align-items-center pt-5">
+                    <div style="border: 2px solid black; background-color: white; border-radius: 15px; padding: 20px;">
+                        <img src="public/skate-placeholders/ninja-skate.svg" alt="live rendering of the options selected" className="skate-designer-rendering"/>
+                    </div>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-center pt-5">
+                <a href="landingPageBootstrap.html" className="btn signin-button-secondary">Cancel</a>
+                <a href="skateView.html" className="btn signin-button-primary">Add to Skate Garage</a>
+            </div>
+        </div>
+    </main>
+  );
+}
