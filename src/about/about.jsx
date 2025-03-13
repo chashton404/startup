b4f4ec948d8bc83e685266fb44070a935a2c5962
Chase Ashton <156870926+chashton404@users.noreply.github.com>
2025-03-13 10:50:57 -0600

    added websocket functionality, mostly just to learn how it works.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 export function AboutPage() {
+    const [msg, setMsg] = React.useState('...listening');
+
+    React.useEffect(() => {
+        setInterval(() => {
+            const names  = ['bob', 'joe', 'sally', 'jane', 'jim', 'jill', 'jack', 'jenny', 'jacob', 'jessica'];
+            const verbs = ['ran', 'jumped', 'swam', 'flew'];
+            const adverbs = ['quickly', 'slowly', 'gracefully', 'awkwardly'];
+            const randomName = names[Math.floor(Math.random() * names.length)];
+            const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+            const randomAdverb = adverbs[Math.floor(Math.random() * adverbs.length)];
+            const newMsg = `${randomName} ${randomVerb} ${randomAdverb}`;
+            setMsg(console.log(newMsg) || newMsg);
+        }, 5000);
+    });
+
     return(
         <main style={{height: 'calc(100vh - 200px)'}}>
             <div className="container">
@@ -12,6 +27,7 @@ export function AboutPage() {
                 <p style={{textAlign: 'center'}}>
                     This is a simple application that allows users to design their own skates. Users can choose from a variety of colors and designs to create a custom skate.
                 </p>
+                <p style={{textAlign: 'center'}}> {msg} </p>
             </div>
         </main>
     );
