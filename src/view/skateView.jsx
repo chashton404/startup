3c52a34a36919dc06d26e8355549f8a64619e0d1
Chase Ashton <156870926+chashton404@users.noreply.github.com>
2025-03-26 18:31:37 -0600

    added a modal for an error and deleting a skate

--- src/view/skateView.jsx
+++ src/view/skateView.jsx
@@ -2,23 +2,106 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import { RollerSkate } from './rollerskate';
+import './modal.css';
 
+//We make a custom confirmation modal for if the user wants to delete the skate
+function ConfirmDeleteModal({ isOpen, onConfirm, onCancel}) {  //We pass in the props isOpen, onConfirm, and onCancel
+    if (!isOpen) {
+        return null; //If we recieve that isOpen is false, the component will not render
+    }
 
-const skates = [
-    { skateName: 'one', skateStatus: 'equipped'},
-    { skateName: 'two', skateStatus: 'not equipped'},
-    { skateName: 'three', skateStatus: 'not equipped'}
-];
+    return (
+        <div className="modal-overlay">
+            <div className="modal-card">
+                <h3>Are you sure you want to delete this skate?</h3>
+                <div className="button-container">
+                    <button className="btn cancel-button" onClick={onCancel}>Cancel</button>
+                    <button className="btn confirm-button" onClick={onConfirm}>Confirm</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-const equippedSkateIndex = 0;
+function ErrorModal({ isOpen, onCancel}) {
+    if (!isOpen) {
+        return null;
+    }
 
-function equipSkate(skateName) {
-    console.log('equipSkate called on ' + skateName);
+    return (
+        <div className="modal-overlay">
+            <div className="modal-card">
+                <h3>You cannot delete an equipped skate</h3>
+                <p style={{ textAlign: 'center' }}>Please Unequip the skate and try again</p>
+                <div className="button-container">
+                    <button className="btn cancel-button" onClick={onCancel}>Ok</button>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export function SkateView() {
+    const [skates, setSkates] = React.useState([
+        { skateName: 'one', skateStatus: 'equipped'},
+        { skateName: 'two', skateStatus: 'not equipped'},
+        { skateName: 'three', skateStatus: 'not equipped'},
+        { skateName: 'four', skateStatus: 'not equipped'},
+        { skateName: 'five', skateStatus: 'not equipped'}
+    ]);
+
+    const [confirmOpenDeleteModal, setConfirmOpenDeleteModal] = React.useState(false);
+    const [errorModalOpen, setErrorModalOpen] = React.useState(false);
+    const [skateToDelete, setSkateToDelete] = React.useState(null);
+
+    function deleteSkate(index) {
+        setSkateToDelete(index);
+        setConfirmOpenDeleteModal(true);
+    }
+
+    function confirmDelete() {
+        const newSkates = skates.filter((skate, index) => index !== skateToDelete);
+        setSkates(newSkates);
+        setConfirmOpenDeleteModal(false);
+        setSkateToDelete(null);
+    }
+
+    function cancelDelete() {
+        setConfirmOpenDeleteModal(false);
+        setSkateToDelete(null);
+    }
+    
+    function openErrorModal() {
+        console.log('openErrorModal called');
+        setErrorModalOpen(true);
+    }
+
+    function closeErrorModal() {
+        setErrorModalOpen(false);
+    }
+    
+    function equipSkate(index) {
+        console.log('equipSkate called on skate with the index of' + index);
+
+        const newSkates = skates.map((skate, i) => {
+            if (i === index) {
+                return {...skate, skateStatus: 'equipped'};
+            } else {
+                return {...skate, skateStatus: 'not equipped'};
+            }
+        });
+
+        setSkates(newSkates);
+    }
+    
 return (
     <main style={{ display: 'block' }}>
+        <ConfirmDeleteModal isOpen={confirmOpenDeleteModal} onConfirm={confirmDelete} onCancel={cancelDelete} /> {/* Here we render the confirmModal that we made telling it 
+                                                                                                        to not load and when it is truly deleted it should use
+                                                                                                        the confirmDelete function and when it it's canceled it 
+                                                                                                        should use the cancelDelete function  */}
+        <ErrorModal isOpen={errorModalOpen} onCancel={closeErrorModal} />
         <div className="row-md-2 justify-content-center p-3">
             <div className="col">
                 <NavLink to="/landing" className="btn signin-button-primary">Back</NavLink>
@@ -34,11 +117,11 @@ return (
                         <img src="public/skate-placeholders/cool-mint-skate-placeholder.svg" alt="Skate Image" className="img-fluid" />
                         <h3>{skate.skateName}</h3>
                         {skate.skateStatus === 'equipped' ? (
-                            <button className="btn signin-button-secondary" disabled>Equipped</button>
+                            <button className="btn signin-button-secondary mb-1" disabled>Equipped</button>
                         ) : (
-                            <button className="btn signin-button-primary" onClick={() => equipSkate(skate.skateName)}>Equip</button>
+                            <button className="btn signin-button-primary mb-1" onClick={() => equipSkate(index)}>Equip</button>
                         )}
-                        <button className="btn signin-button-secondary">Delete</button>
+                        <button className="btn signin-button-secondary" onClick={skate.skateStatus === 'equipped' ? (() => openErrorModal()) : (() => deleteSkate(index))}>Delete</button>
                     </div> 
                 </div>
             ))}
